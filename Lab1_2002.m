04becd92952c5817fed400edef97750d35dccbba
nbottieri <70664874+nbottieri@users.noreply.github.com>
2020-09-16 12:49:57 -0600

    Add files via upload

---
+++ Lab1_2002.m
@@ -0,0 +1,17 @@
+% ASEN 2002 Thermodynamics Design Laboratory Assignment: Atmospheric
+% Satellites
+
+% Variables
+
+mass_payload = 500; % [kg]
+altitude = 25000; %[m] (+/- 1000m)
+FS = 1.5; % Factor of Safety
+Gage_Pressure = 10; % [Pa] (diffence of pressure of balloon and atmosphere
+K = 8.31432; % [Nm/kmol] Universal Gas Constant
+thickness = 2.54*10^-6; % [m] thickness of balloon material
+density_material = 0;
+YS = 27.6*10^6 % [PA] Yield Strength of Material
+
+% Equations:
+
+r = (2*YS*thickness)/(Gage_Pressure*FS)
