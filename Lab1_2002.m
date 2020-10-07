066e95023d0dfe7bf5792a4a22f175ded2d3ea5f
matthewj <matthewj0717@gmail.com>
2020-10-07 15:08:23 -0600

    Claculated daytime values using ideal gas law based on Professor's advice

--- Lab1_2002.m
+++ Lab1_2002.m
@@ -15,26 +15,52 @@ FS = 1.5; % Factor of Safety
 YS = 27.6*10^6; % [Pa]
 
 % Gas properties
-gage_pressure = 10; % [Pa]
-R = 8.314; % [Nm/kmol]
+gage_pressure = 10;      % [Pa]
+R = 8.314;               % [Nm/kmol]
 molar_mass_helium = 4.0026e-03; % [kg/mol]
-R_helium = 2077.1; % [J/kgK]
+R_helium = 2077.1;       % [J/kgK]
 
-day_temp = 272.5641;
-night_temp = 179.7945;
+% Radiation Constants
+sigmaSB = 5.670*(10^-8); % [J/(K^4m^2s)] - Stephan Boltzman constant
+alphasb = .6;            % Absortivity of the sun-balloon system
+epsilonb = .8;           % Emissivity of the balloon
+alphaeb = epsilonb;      % Absortivity of the earth-balloon system
+qsun = 1353;             % [W/m^2] - Solar irradiance
+qearth = 237;            % [W/m^2] - Earth irradiance
+
+%% Calculate temperature of the balloon at day and night
+temp_day = (((alphasb*qsun)+(alphaeb*qearth))/(epsilonb*sigmaSB*4))^.25;
+temp_night = ((alphaeb*qearth)/(epsilonb*sigmaSB*4))^.25;
 
 %% Calculate atmospheric conditions based on the 1976 standard atmosphere at 25km
-[temp_25km, speed_of_sound_25km, pressure_25km, density_25km] = atmoscoesa(altitude, 'None'); % [k, m/s, Pa, kg/m^3]
+[temp_25km, speed_of_sound_25km, pressure_25km, density_25km] = ...
+    atmoscoesa(altitude, 'None'); % [k, m/s, Pa, kg/m^3]
 
 %% Calculate the density of helium at 25km conditions
-density_helium = (pressure_25km + 10)/(R_helium * night_temp); % [kg/m^3]
+density_helium_day = (pressure_25km + 10)/(R_helium * temp_day); % [kg/m^3]
+density_helium_night = (pressure_25km + 10)/(R_helium * temp_night); % [kg/m^3]
 
-%% Find radius of balloon
-radius = nthroot((mass_payload/((((4*pi)/3)*density_25km)-(((4*pi)/3)*density_helium)-(4*pi*density_material*((gage_pressure*FS)/(2*YS))))), 3); % [m]
+%% Calculate radius of balloon at night (launch time)
+radius_night = nthroot((mass_payload/((((4*pi)/3)*density_25km)-(((4*pi)/3)...
+    *density_helium_night)-((4*pi*density_material*gage_pressure*FS)/(2*YS)))), 3); % [m]
+
+%% Calculate mass of balloon material
+mass_material = (4*pi*density_material*gage_pressure*FS*(radius_night^3))/(2*YS); % [kg]
+
+%% Calculate # of moles  and mass of helium present at night
+volume_night = (4/3)*pi*radius_night^3; % [m^3]
+moles_night = ((pressure_25km + gage_pressure)*volume_night)/(R*temp_night); % [mol]
+mass_helium_night = moles_night * molar_mass_helium; % [kg]
+
+%% Calcualte the volume of the helium at the daytime temperature
+% Note that pressure and # of moles remains constant as temperature
+% increases, thus volume must increase. We'll vent gas later
 
 
 
 
+calculated_air_density_night = (density_helium_night*volume_night + ((4*pi*density_material*gage_pressure*FS*radius_night^2)/(2*YS)) + mass_payload)/volume_night
+calculated_air_density_day = (density_helium_day*volume_day + ((4*pi*density_material*gage_pressure*FS*radius_day^2)/(2*YS)) + mass_payload)/volume_day
 
 
 
