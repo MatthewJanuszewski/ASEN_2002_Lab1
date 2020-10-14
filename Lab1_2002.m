1f6d0047980fccfb7060f2726a5a7c02ff3f54d5
matthewj <matthewj0717@gmail.com>
2020-10-14 14:57:53 -0600

    Updated venting to account for reduction in helium mass

--- Lab1_2002.m
+++ Lab1_2002.m
@@ -3,7 +3,9 @@
 clc
 clear
 close all
+
 %% Constants
+
 % Flight properties
 mass_payload = 500; % [kg]
 altitude = 25000; %[m]
@@ -28,40 +30,63 @@ alphaeb = epsilonb;      % Absortivity of the earth-balloon system
 qsun = 1353;             % [W/m^2] - Solar irradiance
 qearth = 237;            % [W/m^2] - Earth irradiance
 
+
 %% Calculate temperature of the balloon at day and night
-temp_day = (((alphasb*qsun)+(alphaeb*qearth))/(epsilonb*sigmaSB*4))^.25;
-temp_night = ((alphaeb*qearth)/(epsilonb*sigmaSB*4))^.25;
+temp_day = (((alphasb*qsun)+(alphaeb*qearth))/(epsilonb*sigmaSB*4))^.25; % [K]
+temp_night = ((alphaeb*qearth)/(epsilonb*sigmaSB*4))^.25; % [K]
+fprintf(['Calculated temperatures using stefan boltzmann equations:',...
+    '\ntemp_day: %.3f K, temp_night: %.3f K\n\n'], temp_day, temp_night);
+
 
 %% Calculate atmospheric conditions based on the 1976 standard atmosphere at 25km
 [temp_25km, speed_of_sound_25km, pressure_25km, density_25km] = ...
     atmoscoesa(altitude, 'None'); % [k, m/s, Pa, kg/m^3]
+fprintf(['Atmospheric conditions found using 1976 standard atmosphere:'...
+    '\ntemp_25km: %.3f K, speed_of_sound_25km: %.3f m/s, pressure_25km: '...
+    '%.3f Pa, density_25km: %.3f kg/m^3\n\n'],... 
+    temp_25km, speed_of_sound_25km, pressure_25km, density_25km);
+
 
 %% Calculate the density of helium at 25km conditions
 density_helium_day = (pressure_25km + 10)/(R_helium * temp_day); % [kg/m^3]
 density_helium_night = (pressure_25km + 10)/(R_helium * temp_night); % [kg/m^3]
+fprintf(['Calculated density of helium using the ideal gas law\n'...
+    'density_helium_day: %.6f kg/m^3, density_helium_night: '...
+    '%.6f kg/m^3\n\n'], density_helium_day, density_helium_night);
+
 
 %% Calculate radius of balloon at night (launch time)
 radius_night = nthroot((mass_payload/((((4*pi)/3)*density_25km)-(((4*pi)/3)...
     *density_helium_night)-((4*pi*density_material*gage_pressure*FS)/(2*YS)))), 3); % [m]
+fprintf(['Radius at night calculated using the formula derrived in design specifications:\n'...
+    'radius_night: %.3f m\n\n'], radius_night);
 
 %% Calculate mass of balloon material
 mass_material = (4*pi*density_material*gage_pressure*FS*(radius_night^3))/(2*YS); % [kg]
+fprintf('Mass of the balloon material:\nmass_material: %.3f kg\n\n', mass_material);
+
 
 %% Calculate # of moles  and mass of helium present at night
 volume_helium_night = (4/3)*pi*radius_night^3; % [m^3]
 moles_helium_night = ((pressure_25km + gage_pressure)*volume_helium_night)/(R*temp_night); % [mol]
 mass_helium_night = moles_helium_night * molar_mass_helium; % [kg]
+fprintf(['Extent of the helium gas at night:\n',...
+    'volume_helium_night: %.3f m^3, moles_helium_night: %.3f mol, mass_helium_night %.3f kg\n\n'],...
+    volume_helium_night, moles_helium_night, mass_helium_night);
 
-%% Calculate the volume of the helium at the daytime temperature
-% Note that pressure and # of moles remains constant as temperature
-% increases, thus volume must increase. We'll vent gas later
-volume_helium_day = (moles_helium_night*R*temp_day)/(pressure_25km + gage_pressure);
 
 %% Calculate how much helium we need to vent
-volume_delta = volume_helium_day - volume_helium_night;
-moles_delta = ((pressure_25km + gage_pressure)*volume_delta)/(R*temp_day);
-mass_delta = moles_delta*molar_mass_helium;
+mass_helium_day = (mass_payload+mass_material)/((density_25km/density_helium_day)-1);
+mass_delta = mass_helium_night - mass_helium_day;
+fprintf(['Mass of daytime balloon after venting:\n',...
+    'mass_helium_day: %.3f kg, mass_delta: %.3f kg\n\n'],...
+    mass_helium_day, mass_delta);
 
-%% Calculate final (daytime) mass of helium
-mass_helium_day = mass_helium_night-mass_delta;
+
+%% Calculate the volume of the daytime balloon
+volume_helium_day = mass_helium_day/density_helium_day;
+volume_delta = volume_helium_night - volume_helium_day;
+fprintf(['Volume of daytime balloon after venting:\n',...
+    'volume_helium_day: %.3f m^3, volume_delta: %.3f m^3\n\n'],...
+    volume_helium_day, volume_helium_night);
 
