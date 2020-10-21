c5111ef34cd42cecc3303d19793e6d06ac980dec
matthewj <matthewj0717@gmail.com>
2020-10-21 13:01:56 -0600

    Moved lab1 files to their own folder and created lab 2 folder

--- Ideal Gas Functions/idealGasSolveForN.m
+++
@@ -1,8 +0,0 @@
-function P = idealGasSolveForP(v, n, T)
-    % Solves ideal gas equation for P given V, n , T
-    % Load Constants
-    
-    % Solve ideal gas law for pressure
-    P = (n*R*T)/v;
-    return
-    
